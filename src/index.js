04304557da7ed98bfc9c0ca702c986eb7369741d
ZhangChuan <zc1415926@126.com>
2019-12-27 09:13:46 +0800

    引入简易中间件，配置redux devtool

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 //引入redux中的createStore方法以生成一个核心数据存储器
 //combineReducer可以合并多个reducer
-import {createStore, combineReducers} from 'redux';
+//引入applyMiddleware以加载middleware, 引入composer配置redux devtool
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 //引入react和redux连接用的包装器Provicer
 import {Provider} from 'react-redux'; 
 //引入我们自己建立的数据处理器(拆分后)
@@ -19,8 +20,23 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
+//新建一个简易的中间件
+const logger = store =>{
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+//使用composerEnhancers配置redux devtool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 //生成核心数据存储器，注册数据处理器reducer（改成rootReducer）
-const store = createStore(rootReducer)
+//引入logger中间件，使用composerEnhancers配置redux devtool
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 //使用Provider将react和redux连接起来，指定store为核心数据存储器
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/store/actions/actions.js
@@ -0,0 +1,47 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBSTRACT = 'SUBSTRACT';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
+
+//添加action creator functions
+export const increment = () => {
+    return{
+        type: INCREMENT
+    };
+};
+
+export const decrement = () => {
+    return{
+        type: DECREMENT
+    };
+};
+
+export const add = (value) => {
+    return{
+        type: ADD,
+        val: value
+    };
+};
+
+export const substract = (value) => {
+    return{
+        type: SUBSTRACT,
+        val: value
+    };
+};
+
+export const storeResult = (res) => {
+    return{
+        type: STORE_RESULT,
+        result: res
+    };
+};
+
+export const deleteResult = (resElId) => {
+    return{
+        type: DELETE_RESULT,
+        resultElId: resElId
+    };
+};
